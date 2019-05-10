3690b0d7a91d184774c1bfb3a68ceeff55ceedf2
xiaoyueyue165 <1656800216@qq.com>
2019-05-10 22:36:42 +0800

    add .editorconfig

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-
 var useSDK = require("./lib/Cloud-code");
 var other = require("./lib/other");
 
-
-
-
-
 module.exports = {
-    useSDK: useSDK,
-    other: other
-}
+  useSDK: useSDK,
+  other: other
+};
